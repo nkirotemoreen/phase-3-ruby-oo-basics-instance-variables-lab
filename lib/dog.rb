b3c135358b4a43975269cafe58150855e94bd322
nkirotemoreen <nkirotemoreen70@gmail.com>
2022-10-23 02:16:13 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
-
+#setter method
+    def name=(dogs_name)
+        @this_dogs_name = dogs_name
+    end
+    #getter method
+    def name
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+lassie .name = "Lassie"
+
+puts  lassie.name
